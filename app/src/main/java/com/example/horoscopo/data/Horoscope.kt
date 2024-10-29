8dd385908067e0f6fd8eda1fb623bc65522cdefc
Jesus Sierralaya <random.music.1234567890@gmail.com>
2024-10-29 17:14:34 +0100

    copiando

---
+++ app/src/main/java/com/example/horoscopo/data/Horoscope.kt
@@ -0,0 +1,7 @@
+package com.example.horoscopo.data
+
+class Horoscope (val id: String, val name: Int, val dates: Int, val image: Int ){
+
+
+
+}
---
+++ app/src/main/java/com/example/horoscopo/data/Horoscopo_provider.kt
@@ -0,0 +1,12 @@
+package com.example.horoscopo.data
+
+class Horoscopo_provider {
+
+    val horoscopeList: List<Horoscope> = listOf()
+            
+
+
+    fun findAll(): List<Horoscope>{}
+
+    fun findById(id:String): Horoscope{}
+}
